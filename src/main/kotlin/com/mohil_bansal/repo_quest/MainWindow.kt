a0ec5d7f55db312d0d540b5697e66f7e6f86bc5d
Mohil Bansal <lucifer1111ronaldo@gmail.com>
2025-02-13 16:18:07 +0530

    feat: Initial Setup for the RepoQuest

---
+++ src/main/kotlin/com/mohil_bansal/repo_quest/MainWindow.kt
@@ -0,0 +1,11 @@
+package com.mohil_bansal.repo_quest
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindow
+import com.intellij.openapi.wm.ToolWindowFactory
+
+class MainWindow : ToolWindowFactory {
+    override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
+        TODO("Not yet implemented")
+    }
+}
