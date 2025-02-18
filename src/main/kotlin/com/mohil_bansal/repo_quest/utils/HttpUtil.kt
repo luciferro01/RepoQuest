8653fc7e3af85c375c5112ef6916e8071a8a24f8
Mohil Bansal <lucifer1111ronaldo@gmail.com>
2025-02-18 15:13:24 +0530

    feat: Add proxy support for HTTP requests and implement random proxy selection

--- src/main/kotlin/com/mohil_bansal/repo_quest/utils/HttpUtil.kt
+++ src/main/kotlin/com/mohil_bansal/repo_quest/utils/HttpUtil.kt
@@ -3,28 +3,84 @@ package com.mohil_bansal.repo_quest.utils
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import java.io.IOException
+import java.net.Proxy
+import java.net.InetSocketAddress
+import kotlin.random.Random
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 object HttpUtils {
 
+//    private val proxy = Proxy(Proxy.Type.HTTP, InetSocketAddress("185.105.102.189", 8080))
     private val client = OkHttpClient()
+    private const val PROXY_LIST_URL = "https://raw.githubusercontent.com/LoneKingCode/free-proxy-db/refs/heads/main/proxies/http.json"
+
 
     @JvmStatic
     @Throws(IOException::class)
     fun sendGet(url: String): String {
-//        return sendGet(url, emptyMap())
         val request = Request.Builder()
             .url(url)
             .header(
                 "User-Agent",
                 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
             )
+            .header("Referer", "https://mvnrepository.com/")
+            .header("Accept-Language", "en-US,en;q=0.5")
+            .header("Connection", "keep-alive")
+            .header("Upgrade-Insecure-Requests", "1")
             .build()
 
         client.newCall(request).execute().use { response ->
+            if (response.code == 403) {
+                val proxy = getRandomProxy()
+                return sendGetWithProxy(url, proxy)
+            }
             if (!response.isSuccessful) throw IOException("Unexpected code $response")
-            return response.body?.string() ?: ""
+            return response.body.string()
         }
     }
+
+    @JvmStatic
+    @Throws(IOException::class)
+    fun sendGetWithProxy(url: String, proxy: Proxy): String {
+        val clientWithProxy = OkHttpClient.Builder().proxy(proxy).build()
+        val request = Request.Builder()
+            .url(url)
+            .header(
+                "User-Agent",
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+            )
+            .header("Referer", "https://mvnrepository.com/")
+            .header("Accept-Language", "en-US,en;q=0.5")
+            .header("Connection", "keep-alive")
+            .header("Upgrade-Insecure-Requests", "1")
+            .build()
+
+        clientWithProxy.newCall(request).execute().use { response ->
+            if (!response.isSuccessful) throw IOException("Unexpected code $response")
+            return response.body.string()
+        }
+    }
+
+    @JvmStatic
+    @Throws(IOException::class)
+    fun getRandomProxy(): Proxy {
+        val request = Request.Builder().url(PROXY_LIST_URL).build()
+        client.newCall(request).execute().use { response ->
+            if (!response.isSuccessful) throw IOException("Failed to fetch proxy list")
+            val proxyListJson = response.body.string()
+            val proxyListType = object : TypeToken<List<ProxyInfo>>() {}.type
+            val proxyList: List<ProxyInfo> = Gson().fromJson(proxyListJson, proxyListType)
+            val randomProxy = proxyList[Random.nextInt(proxyList.size)]
+            return Proxy(Proxy.Type.HTTP, InetSocketAddress(randomProxy.ip, randomProxy.port))
+        }
+    }
+
+    data class ProxyInfo(
+        val ip: String,
+        val port: Int
+    )
 
     /**
      * @param url Request URL
